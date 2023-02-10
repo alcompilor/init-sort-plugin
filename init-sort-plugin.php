c286e52eb18aafc2bc48aa08681639c13c180d2f
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2023-02-10 17:14:43 +0100

    version num updated

--- init-sort-plugin.php
+++ init-sort-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Init & Sort Variations for Woocommerce
 Plugin URI: https://ahmed.dev
 description: A woocommerce plugin that automatically enables stock management for all products variations, additionally this plugin sorts products based on the variations stock count. It does all of that once every 24 hours.
-Version: 1.1
+Version: 1.2
 Author: Ahmed Abbasi
 Author URI: https://ahmed.dev
 License: All rights reserved for Ahmed Abbasi © 2023. Using, sharing or editing this plugin without any permission from the developer is strictly prohibited.
